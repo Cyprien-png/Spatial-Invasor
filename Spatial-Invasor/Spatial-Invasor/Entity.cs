c7ec97ad3a0bc0b8b1eb6ab65ac06e5c4d44da8e
benjaminMum <61788752+benjaminMum@users.noreply.github.com>
2021-12-10 14:54:22 +0100

    Première refonte de "Entity"

--- Spatial-Invasor/Spatial-Invasor/Entity.cs
+++ Spatial-Invasor/Spatial-Invasor/Entity.cs
@@ -7,40 +7,33 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Spatial_Invasor
 {
-    public class Entity
+    public class Entity : DrawableGameComponent
     {
         public List<Rectangle> SheetPositions;
 
-        public float Speed;
-        public Vector2 Position;
+        protected float Speed;
+        protected Vector2 Position;
         public Rectangle Hitbox;
 
         public SpriteBatch SpriteBatch;
         public GraphicsDeviceManager Graphics;
-        public Texture2D SpriteSheet;
+        protected Texture2D SpriteSheet;
 
 
-        public Entity(GraphicsDeviceManager Graphics, SpriteBatch SpriteBatch, Vector2 Position, Texture2D Spritesheet, Rectangle spriteSheetPosition)
+        public Entity(Game game) : base(game)
         {
+            /*
             this.Graphics = Graphics;
             this.SpriteBatch = SpriteBatch;
             this.Position = Position;
             SpriteSheet = Spritesheet;
             Hitbox = new Rectangle((int)Position.X, (int)Position.Y, spriteSheetPosition.Width, spriteSheetPosition.Height);
+            */
         }
 
-
-        public virtual void Initialize()
-        {
-
+        protected override void LoadContent() {
+            SpriteSheet = Game.Content.Load<Texture2D>("main-spritesheet");
         }
-
-        public virtual void Update(GameTime gameTime)
-        {
-
-        }
-
-        public virtual void Draw() { }
     }
 
 }
